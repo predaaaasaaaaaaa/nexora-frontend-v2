9ea4d9cc1c3cfcf94962ab58e08e8f73d5bc5721
predaaaasaaaaaaa <sheiladrivecontent@gmail.com>
2026-05-31 12:52:00 -0400

    chore: exclude private routes from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
-exclude: ['/dashboard', '/settings', '/coach', '/scheduler', '/ideas', '/analytics', '/login', '/signup', '/forgot-password', '/reset-password'],
 module.exports = {
-    siteUrl: 'https://nexora-ai.org',
-    generateRobotsTxt: false,
-    outDir: './public',
-  }
+  siteUrl: 'https://nexora-ai.org',
+  generateRobotsTxt: false,
+  outDir: './public',
+  exclude: ['/dashboard', '/settings', '/coach', '/scheduler', '/ideas', '/analytics', '/login', '/signup', '/forgot-password', '/reset-password'],
+}
